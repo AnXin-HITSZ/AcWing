3b0df8040330c8ec425f480c51f20c3ccf383eb0
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-01-09 18:16:18 +0800

    Notes - 20260109

---
+++ SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
@@ -0,0 +1,17 @@
+package com.anxin_hitsz;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.anxin_hitsz
+ * Description:
+ *
+ * @Author AnXin
+ * @Create 2026/1/9 17:24
+ * @Version 1.0
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
