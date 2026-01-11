075876c416021b7c39478e6792e192c211cb5698
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-01-11 21:00:29 +0800

    Notes - 20260111

--- SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
+++ SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
@@ -17,40 +17,29 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        while (N != 0) {
-            String[] str = new String[N];
-            for (int i = 0; i < N; i++) {
-                str[i] = sc.next();
-            }
-            boolean flag = true;
-            int minIndex = 201;
-            for (int i = 0; i < N; i++) {
-                if (str[i].length() < minIndex) {
-                    minIndex = str[i].length();
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public static class Solution {
+        public ListNode reverseList(ListNode head) {
+            if (head.next == null) {
+                return head;
+            } else {
+                ListNode pre = head;
+                ListNode tail = head.next;
+                while (tail != null) {
+                    pre.next.next = pre;
+                    pre = tail;
+                    tail = tail.next;
                 }
-            }
-            for (int i = 0; i < minIndex; i++) {
-                for (int j = 0; j < N - 1; j++) {
-                    if (str[j].charAt(str[j].length() - 1 - i) == str[j + 1].charAt(str[j + 1].length() - 1 - i)) {
-                        flag = true;
-                    } else {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag == false) {
-                    if (i == 0) {
-                        System.out.println();
-                    } else {
-                        for (int k = str[0].length() - i; k < str[0].length(); k++) {
-                            System.out.print(str[0].charAt(k));
-                        }
-                    }
-                    break;
-                }
+                head.next = null;
+                return pre;
             }
         }
     }
