51f5b3be84809c26fdc0568f45d93e16d9c01976
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-01-11 18:08:12 +0800

    Notes - 20260111

--- SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
+++ SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
@@ -2,6 +2,8 @@ package com.anxin_hitsz;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * ClassName: ${NAME}
@@ -15,11 +17,41 @@ import java.io.InputStreamReader;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        String[] strs = {"aaa", "bbb", "ccc"};
-        for (String str: strs) {
-            System.out.println(str);
-        }
+        Scanner sc = new Scanner(System.in);
 
-        int a = Math.abs(5);
+        int N = sc.nextInt();
+        while (N != 0) {
+            String[] str = new String[N];
+            for (int i = 0; i < N; i++) {
+                str[i] = sc.next();
+            }
+            boolean flag = true;
+            int minIndex = 201;
+            for (int i = 0; i < N; i++) {
+                if (str[i].length() < minIndex) {
+                    minIndex = str[i].length();
+                }
+            }
+            for (int i = 0; i < minIndex; i++) {
+                for (int j = 0; j < N - 1; j++) {
+                    if (str[j].charAt(str[j].length() - 1 - i) == str[j + 1].charAt(str[j + 1].length() - 1 - i)) {
+                        flag = true;
+                    } else {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag == false) {
+                    if (i == 0) {
+                        System.out.println();
+                    } else {
+                        for (int k = str[0].length() - i; k < str[0].length(); k++) {
+                            System.out.print(str[0].charAt(k));
+                        }
+                    }
+                    break;
+                }
+            }
+        }
     }
 }
