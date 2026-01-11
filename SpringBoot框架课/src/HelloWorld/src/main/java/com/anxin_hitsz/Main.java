2c061bb16e9dffeac0c99b6cc999bd65a4122ba5
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-01-11 16:27:48 +0800

    Notes - 20260111

--- SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
+++ SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
@@ -1,6 +1,7 @@
 package com.anxin_hitsz;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * ClassName: ${NAME}
@@ -8,25 +9,17 @@ import java.util.Scanner;
  * Description:
  *
  * @Author AnXin
- * @Create 2026/1/9 17:24
+ * @Create 2026/1/2 19:18
  * @Version 1.0
  *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] n = new int[6];
-        for (int i = 0; i < 6; i++) {
-            n[i] = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        String[] strs = {"aaa", "bbb", "ccc"};
+        for (String str: strs) {
+            System.out.println(str);
         }
 
-        int count = 0;
-        for (int i = 0; i < 6; i++) {
-            if (n[i] > 0) {
-                count++;
-            }
-        }
-
-        System.out.println(count);
+        int a = Math.abs(5);
     }
 }
