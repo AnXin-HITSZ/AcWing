fa81b7038b721f21a85a069865f173c36fc7ccbf
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-01-12 12:13:15 +0800

    Notes - 20260112

--- SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
+++ SpringBoot框架课/src/HelloWorld/src/main/java/com/anxin_hitsz/Main.java
@@ -16,31 +16,32 @@ import java.util.Scanner;
  *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        triTuple[] t = new triTuple[N];
+        int[] tmpX = new int[N];
+        for (int i = 0; i < N; i++) {
+            t[i].x = sc.nextInt();
+            tmpX[i] = t[i].x;
+            t[i].y = sc.nextDouble();
+            t[i].z = sc.next();
+        }
 
-    }
-
-    public static class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-    }
-
-    public static class Solution {
-        public ListNode reverseList(ListNode head) {
-            if (head.next == null) {
-                return head;
-            } else {
-                ListNode pre = head;
-                ListNode tail = head.next;
-                while (tail != null) {
-                    pre.next.next = pre;
-                    pre = tail;
-                    tail = tail.next;
+        Arrays.sort(tmpX);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (t[j].x == tmpX[i]) {
+                    System.out.printf("%d %.2f %s\n", t[j].x, t[j].y, t[j].z);
+                    break;
                 }
-                head.next = null;
-                return pre;
             }
         }
     }
+}
+
+class triTuple {
+    int x;
+    double y;
+    String z;
 }
